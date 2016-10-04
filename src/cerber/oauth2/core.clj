b35f6cdbde06adbe9e160643df8be7c49cd1d752
Michal Buczko <michal.buczko@gmail.com>
2016-10-04 14:26:51 +0200

    initial public API

---
+++ src/cerber/oauth2/core.clj
@@ -0,0 +1,40 @@
+(ns cerber.oauth2.core
+  (:require [cerber.stores.client :as client]
+            [cerber.stores.token :as token]))
+
+;; tokens
+
+(defn find-tokens-by-client
+  [client]
+  (when-let [client-id (:id client)]
+    (token/find-by-pattern [client-id "refresh" nil])))
+
+(defn find-tokens-by-user
+  [user]
+  (when-let [login (:login user)]
+    (token/find-by-pattern [nil "refresh" nil login])))
+
+(defn revoke-tokens
+  ([client]
+   (when-let [client-id (:id client)]
+     (token/revoke-by-pattern [client-id nil])))
+  ([client login]
+   (when-let [client-id (:id client)]
+     (token/revoke-by-pattern [client-id nil nil login]))))
+
+;; clients
+
+(defn find-client
+  [client-id]
+  (client/find-client client-id))
+
+(defn create-client
+  [homepage redirects scopes grants authorities]
+  (client/create-client homepage redirects scopes grants authorities))
+
+(defn modify-client
+  [client])
+
+(defn delete-client
+  [client]
+  (client/revoke-client (:id client)))
